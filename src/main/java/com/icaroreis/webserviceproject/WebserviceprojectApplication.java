2752f5e310e67ec3cc24d81c6cc98e15242b525a
Ícaro Reis <icaroreis1982@gmail.com>
2025-01-31 18:07:06 -0300

    criação das classes User entity and UserResource. aplicação testada e requisições funcionando!

--- src/main/java/com/icaroreis/webserviceproject/WebserviceprojectApplication.java
+++ src/main/java/com/icaroreis/webserviceproject/WebserviceprojectApplication.java
@@ -9,6 +9,7 @@ public class WebserviceprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceprojectApplication.class, args);
 
+
 	}
 
 }
---
+++ src/main/java/com/icaroreis/webserviceproject/resources/UserResource.java
@@ -0,0 +1,27 @@
+package com.icaroreis.webserviceproject.resources;
+
+
+// classe Resouce
+/* responsável por lidar com as requisições HTTP (GET, POST, PUT, DELETE)
+relacionadas aos usuários. */
+
+import com.icaroreis.webserviceproject.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/Users") // guia no localHost
+public class UserResource {
+
+
+    // exemplo de requisição
+    @GetMapping
+    public ResponseEntity<User> finAllUsers() {
+        User user = new User(1L,"Mari","mari@","12223","55588");
+        return ResponseEntity.ok().body(user);
+    }
+
+}
