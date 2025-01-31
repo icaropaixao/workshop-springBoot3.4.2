8081c416f8b7833d8fe7b7cd9b007c4202bd447b
Ícaro Reis <icaroreis1982@gmail.com>
2025-01-31 17:35:12 -0300

    Criação do projeto web service com Spring Boot e JPA / Hibernate

---
+++ src/main/java/com/icaroreis/webserviceproject/WebserviceprojectApplication.java
@@ -0,0 +1,14 @@
+package com.icaroreis.webserviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceprojectApplication.class, args);
+
+	}
+
+}
